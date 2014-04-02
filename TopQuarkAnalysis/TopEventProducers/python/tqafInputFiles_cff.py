cbf2d75d976f01b440265aad1f3ffedbe14cd063
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-04-02 17:17:48 +0200

    addpkg TopQuarkAnalysis/Configuration         V06-01-14-04 addpkg TopQuarkAnalysis/Examples              V06-07-10 addpkg TopQuarkAnalysis/TopEventProducers     V06-07-14 addpkg TopQuarkAnalysis/TopEventSelection     V06-07-13 addpkg TopQuarkAnalysis/TopJetCombination     V06-07-10 addpkg TopQuarkAnalysis/TopKinFitter          V06-07-17 addpkg TopQuarkAnalysis/TopObjectResolutions  V06-07-08 addpkg TopQuarkAnalysis/TopTools              V06-07-11

---
+++ TopQuarkAnalysis/TopEventProducers/python/tqafInputFiles_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import pickRelValInputFiles
+#relValTTbar = pickRelValInputFiles(cmsswVersion  = 'CMSSW_4_2_9_HLT1',
+#                                   relVal        = 'RelValTTbar',
+#                                   globalTag     = 'MC_42_V14A',
+#                                   numberOfFiles = 1
+#                                   )
+relValTTbar = ['/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0006/12A43774-08A9-E011-AFDC-00248C0BE01E.root',
+               '/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0006/2A7B62DF-0FA9-E011-B448-001A92811728.root',
+               '/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0006/EC4D7A70-FFA8-E011-8B23-0026189438F4.root',
+               '/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0006/FCD53067-05A9-E011-9521-001A928116E6.root',
+               '/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0007/60F142D3-48A9-E011-B058-001A928116C4.root',
+               '/store/relval/CMSSW_4_2_6/RelValTTbar/GEN-SIM-RECO/START42_V12-v1/0009/5645192C-E7AA-E011-A714-0026189437F8.root']
